5f1641027abd365296d25d9414e1bf58dc393ec6
Володимир Романиця <romanytsiavolodymyr@MacBook-Air-Volodimir.local>
2019-02-19 00:12:30 +0200

    fixed package.json app.js

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use('/api/auth', authRoutes)
 app.use('/api/todos', todosRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist/client'))
+    app.use(express.static('client1/build'))
   
     app.get('*', (req, res) => {
       res.sendFile(
         path.resolve(
-          __dirname, 'client', 'dist', 'client', 'index.html'
+          __dirname, 'client1', 'build', 'index.html'
         )
       )
     })
